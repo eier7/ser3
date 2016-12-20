a90ad15dac1a4f0a813cda1003a7728845b5ae56
eier7 <eivind.ervik@gmail.com>
2016-12-20 15:11:54 +0100

    div

--- ser3.py
+++ ser3.py
@@ -113,14 +113,17 @@ def GUI():
 
         for m in range(len(menu)):
             if menucursor[0] == 0: 
-                screen.addstr(height-1, 20, menu[0][serialport], curses.color_pair(3))
-                screen.addstr(height-1, 28, menu[1][baud], curses.color_pair(2))
+                screen.addstr(height-1, 17, menu[0][serialport], curses.color_pair(3))
+                screen.addstr(height-1, 24, menu[1][baud], curses.color_pair(2))
+                screen.addstr(height-1, 31, menu[1][0], curses.color_pair(2))
             elif menucursor[0] == 1:
                 screen.addstr(height-1, 20, menu[0][serialport], curses.color_pair(2))
                 screen.addstr(height-1, 28, menu[1][baud], curses.color_pair(3))
+                screen.addstr(height-1, 31, menu[1][0], curses.color_pair(2))
             elif menucursor[0] == 2:
                 screen.addstr(height-1, 20, menu[0][serialport], curses.color_pair(2))
                 screen.addstr(height-1, 28, menu[1][baud], curses.color_pair(2))
+                screen.addstr(height-1, 31, menu[1][0], curses.color_pair(3))
         if serialerror:
             if int(time.time()) % 2 == 0:
                 screen.addstr(height-1, 0, "SERIAL PORT ERROR", curses.color_pair(4))
@@ -185,7 +188,7 @@ def NMEA():
 def GPIObuttons():
     while(True):
         if not GPIO.input(27):
-            gpioq.put("left")
+            gpioq.put("right")
             time.sleep(.1)
         if not GPIO.input(22):
             gpioq.put("down")
